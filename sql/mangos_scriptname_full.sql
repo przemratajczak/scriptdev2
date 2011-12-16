70c2605b9f057ed0d4519ab96541b0c10df00c05
Bearq <bear2493@gmail.com>
2011-12-16 13:54:05 +0100

    Clean sql

--- sql/mangos_scriptname_full.sql
+++ sql/mangos_scriptname_full.sql
@@ -357,13 +357,6 @@ UPDATE creature_template SET ScriptName='boss_flamegor' WHERE entry=11981;
 UPDATE creature_template SET ScriptName='boss_chromaggus' WHERE entry=14020;
 UPDATE creature_template SET ScriptName='boss_victor_nefarius' WHERE entry=10162;
 UPDATE creature_template SET ScriptName='boss_nefarian' WHERE entry=11583;
-UPDATE creature_template SET ScriptName = "mob_red_drakonid" WHERE entry = 14264;
-UPDATE creature_template SET ScriptName = "mob_blue_drakonid" WHERE entry = 14261;
-UPDATE creature_template SET ScriptName = "mob_green_drakonid" WHERE entry = 14262;
-UPDATE creature_template SET ScriptName = "mob_black_drakonid" WHERE entry = 14265;
-UPDATE creature_template SET ScriptName = "mob_bronze_drakonid" WHERE entry = 14263;
-UPDATE creature_template SET ScriptName = "mob_chromatic_drakonid" WHERE entry = 14302;
-UPDATE creature_template SET faction_A = 103, `faction_H` = 103 WHERE `entry` = 14261;
 
 /* BLADE'S EDGE MOUNTAINS */
 UPDATE creature_template SET ScriptName='mobs_bladespire_ogre' WHERE entry IN (19998,20334,21296,21975);
