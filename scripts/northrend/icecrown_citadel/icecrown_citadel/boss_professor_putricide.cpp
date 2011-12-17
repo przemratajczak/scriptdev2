b99139fa229ea3263efeb01eed36515f155c18f6
michalpolko <michalpolko@o2.pl>
2011-12-17 12:11:24 +0100

    [a167] Implemented spells placeholders (timers) and phases for Putricide.

--- scripts/northrend/icecrown_citadel/icecrown_citadel/boss_professor_putricide.cpp
+++ scripts/northrend/icecrown_citadel/icecrown_citadel/boss_professor_putricide.cpp
@@ -26,6 +26,29 @@ EndScriptData */
 
 enum BossSpells
 {
+    SPELL_BERSERK                   = 47008,
+    SPELL_SLIME_PUDDLE_SUMMON       = 70342,
+    SPELL_SLIME_PUDDLE_MISSILE      = 70341,
+    SPELL_SLIME_PUDDLE_AURA         = 70343,
+ // SPELL_SLIME_PUDDLE_TRIGGER      = 71424, // trigger summon spell from target?
+ // SPELL_SLIME_PUDDLE_SUMMON_TRIG  = 71425,
+    SPELL_CHOKING_GAS_BOMB          = 71255,
+    SPELL_CHOKING_GAS_BOMB_AURA     = 71259,
+    SPELL_CHOKING_GAS_BOMB_EXPL_AUR = 71280,
+
+    // phase transitions
+    SPELL_TEAR_GAS_1                = 71615,
+    SPELL_TEAR_GAS                  = 71617,
+    SPELL_TEAR_GAS_2                = 71618,
+    SPELL_TEAR_GAS_PERIODIC_AURA    = 73170,
+    SPELL_TEAR_GAS_CANCEL           = 71620,
+    SPELL_CREATE_CONCOCTION         = 71621,
+    SPELL_GUZZLE_POTIONS            = 71893,
+
+    NPC_GROWING_OOZE_PUDDLE         = 37690,
+    NPC_GROWING_OOZE_PUDDLE_TRIG    = 38234,
+    NPC_CHOKING_GAS_BOMB            = 38159,
+/*
     SPELL_UNSTABLE_EXPERIMENT     = 70351, // Script effect not work on 10154. Spawn manually.
     SPELL_TEAR_GAS                = 71617,
     SPELL_TEAR_GAS_1              = 71615,
@@ -83,6 +106,7 @@ enum BossSpells
     VIEW_1                        = 30881,
     VIEW_2                        = 30881,
     VIEW_3                        = 30993,
+*/
 };
 
 // talks
@@ -113,11 +137,23 @@ static Locations SpawnLoc[]=
     {4417.302246f, 3188.219971f, 389.332520f, 5.102f},  // 2 Putricide Rotface say o=5.102
 };
 
-#define PHASE_ONE 1
-#define PHASE_TRANSITION 2
-#define PHASE_TWO 3
-#define PHASE_THREE 4
+#define POINT_PUTRICIDE_SPAWN 1
 
+enum Phases
+{
+    PHASE_ONE = 1,
+    PHASE_RUNNING_ONE = 2,
+    PHASE_TRANSITION_ONE = 3,
+    PHASE_TWO = 4,
+    PHASE_RUNNING_TWO = 5,
+    PHASE_TRANSITION_TWO = 6,
+    PHASE_THREE = 7
+};
+
+
+/**
+ * boss_professor_putricide
+ */
 struct MANGOS_DLL_DECL boss_professor_putricideAI : public base_icc_bossAI
 {
     boss_professor_putricideAI(Creature* pCreature) : base_icc_bossAI(pCreature)
@@ -127,14 +163,30 @@ struct MANGOS_DLL_DECL boss_professor_putricideAI : public base_icc_bossAI
 
     uint32 m_uiPhase;
 
+    uint32 m_uiHealthCheckTimer;
+    uint32 m_uiTransitionTimer;
+    uint32 m_uiEnrageTimer;
+    uint32 m_uiPuddleTimer;
+    uint32 m_uiUnstableExperimentTimer;
+    uint32 m_uiMalleableGooTimer;
+    uint32 m_uiChokingGasBombTimer;
+
     // used to determine whether he is assisting one of his pupils or having his own encounter
     bool m_bIsAssistingOnly;
 
     void Reset()
     {
-        m_uiPhase = PHASE_ONE;
-        m_bIsAssistingOnly = false;
+        m_uiPhase                   = PHASE_ONE;
+        m_bIsAssistingOnly          = false;
         SetCombatMovement(true);
+
+        m_uiHealthCheckTimer        = 1000;
+        m_uiTransitionTimer         = 20000;
+        m_uiEnrageTimer             = 10 * MINUTE * IN_MILLISECONDS;
+        m_uiPuddleTimer             = 10000;
+        m_uiUnstableExperimentTimer = 20000;
+        m_uiMalleableGooTimer       = 5000;
+        m_uiChokingGasBombTimer     = urand(10000, 15000);
     }
 
     void DamageTaken(Unit *pDealer, uint32 &uiDamage)
@@ -182,6 +234,26 @@ struct MANGOS_DLL_DECL boss_professor_putricideAI : public base_icc_bossAI
             m_pInstance->SetData(TYPE_PUTRICIDE, FAIL);
     }
 
+    void MovementInform(uint32 uiMovementType, uint32 uiData)
+    {
+        if (uiMovementType != POINT_MOTION_TYPE)
+            return;
+
+        if (uiData == POINT_PUTRICIDE_SPAWN)
+        {
+            if (m_uiPhase == PHASE_RUNNING_ONE)
+            {
+                DoCastSpellIfCan(m_creature, SPELL_CREATE_CONCOCTION);
+                m_uiPhase = PHASE_TRANSITION_ONE; // counter for entering phase 2
+            }
+            else if (m_uiPhase == PHASE_RUNNING_TWO)
+            {
+                DoCastSpellIfCan(m_creature, SPELL_GUZZLE_POTIONS);
+                m_uiPhase = PHASE_TRANSITION_TWO; // counter for entering phase 3
+            }
+        }
+    }
+
     void UpdateAI(const uint32 uiDiff)
     {
         // must move teleporting of profesor after assisting to scripts of the other bosses (to their JustDied)
@@ -193,21 +265,195 @@ struct MANGOS_DLL_DECL boss_professor_putricideAI : public base_icc_bossAI
         if (!m_creature->SelectHostileTarget() || !m_creature->getVictim())
             return;
 
+        // Enrage
+        if (m_uiEnrageTimer <= uiDiff)
+        {
+            if (DoCastSpellIfCan(m_creature, SPELL_BERSERK) == CAST_OK)
+            {
+                DoScriptText(SAY_BERSERK, m_creature);
+                m_uiEnrageTimer = 30 * MINUTE * IN_MILLISECONDS;
+            }
+        }
+        else
+            m_uiEnrageTimer -= uiDiff;
+
         switch(m_uiPhase)
         {
             case PHASE_ONE:
+            {
+                // health check
+                if (m_uiHealthCheckTimer <= uiDiff)
+                {
+                    if (m_creature->GetHealthPercent() <= 80.0f)
+                    {
+                        DoCastSpellIfCan(m_creature, SPELL_TEAR_GAS, CAST_TRIGGERED);
+                        DoCastSpellIfCan(m_creature, SPELL_TEAR_GAS_1, CAST_TRIGGERED);
+                        m_creature->GetMotionMaster()->MovePoint(POINT_PUTRICIDE_SPAWN, SpawnLoc[0].x, SpawnLoc[0].y, SpawnLoc[0].z);
+                        m_uiPhase = PHASE_RUNNING_ONE;
+                        return;
+                    }
+                    m_uiHealthCheckTimer = 1000;
+                }
+                else
+                    m_uiHealthCheckTimer -= uiDiff;
+
+                // Slime Puddle
+                if (m_uiPuddleTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiPuddleTimer = 30000;
+                }
+                else
+                    m_uiPuddleTimer -= uiDiff;
+
+                // Unstable Experiment
+                if (m_uiUnstableExperimentTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiUnstableExperimentTimer = 30000;
+                }
+                else
+                    m_uiUnstableExperimentTimer -= uiDiff;
+
+                break;
+            }
+            case PHASE_TRANSITION_ONE:
+            {
+                // 10 seconds every phase transition
+                if (m_uiTransitionTimer <= uiDiff)
+                {
+                    if (DoCastSpellIfCan(m_creature, SPELL_TEAR_GAS_CANCEL) == CAST_OK)
+                    {
+                        m_creature->GetMotionMaster()->Clear();
+                        m_creature->GetMotionMaster()->MoveChase(m_creature->getVictim());
+                        m_uiTransitionTimer = 20000;
+                        m_uiPhase = PHASE_TWO;
+                    }
+                }
+                else
+                    m_uiTransitionTimer -= uiDiff;
+
+                return;
+            }
+            case PHASE_TWO:
+            {
+                // health check
+                if (m_uiHealthCheckTimer <= uiDiff)
+                {
+                    if (m_creature->GetHealthPercent() <= 35.0f)
+                    {
+                        DoCastSpellIfCan(m_creature, SPELL_TEAR_GAS, CAST_TRIGGERED);
+                        DoCastSpellIfCan(m_creature, SPELL_TEAR_GAS_1, CAST_TRIGGERED);
+                        m_creature->GetMotionMaster()->MovePoint(POINT_PUTRICIDE_SPAWN, SpawnLoc[0].x, SpawnLoc[0].y, SpawnLoc[0].z);
+                        m_uiPhase = PHASE_RUNNING_TWO;
+                        return;
+                    }
+                    m_uiHealthCheckTimer = 1000;
+                }
+                else
+                    m_uiHealthCheckTimer -= uiDiff;
+
+                // Slime Puddle
+                if (m_uiPuddleTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiPuddleTimer = 30000;
+                }
+                else
+                    m_uiPuddleTimer -= uiDiff;
+
+                // Unstable Experiment
+                if (m_uiUnstableExperimentTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiUnstableExperimentTimer = 30000;
+                }
+                else
+                    m_uiUnstableExperimentTimer -= uiDiff;
+
+                // Malleable Goo
+                if (m_uiMalleableGooTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiMalleableGooTimer = 20000;
+                }
+                else
+                    m_uiMalleableGooTimer -= uiDiff;
+
+                // Choking Gas
+                if (m_uiChokingGasBombTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiChokingGasBombTimer = urand(25000, 30000);
+                }
+                else
+                    m_uiChokingGasBombTimer -= uiDiff;
+
+                break;
+            }
+            case PHASE_TRANSITION_TWO:
+            {
+                // 10 seconds every phase transition
+                if (m_uiTransitionTimer <= uiDiff)
+                {
+                    m_creature->GetMotionMaster()->Clear();
+                    m_creature->GetMotionMaster()->MoveChase(m_creature->getVictim());
+                    m_uiTransitionTimer = 10000;
+                    m_uiPhase = PHASE_THREE;
+                }
+                else
+                    m_uiTransitionTimer -= uiDiff;
+
+                return;
+            }
+            case PHASE_THREE:
+            {
+                // Slime Puddle
+                if (m_uiPuddleTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiPuddleTimer = 30000;
+                }
+                else
+                    m_uiPuddleTimer -= uiDiff;
+
+                // Malleable Goo
+                if (m_uiMalleableGooTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiMalleableGooTimer = 20000;
+                }
+                else
+                    m_uiMalleableGooTimer -= uiDiff;
+
+                // Choking Gas
+                if (m_uiChokingGasBombTimer <= uiDiff)
+                {
+                    // if (DoCastSpellIfCan() == CAST_OK)
+                        m_uiChokingGasBombTimer = urand(25000, 30000);
+                }
+                else
+                    m_uiChokingGasBombTimer -= uiDiff;
+
+                break;
+            }
+            case PHASE_RUNNING_ONE:
+            case PHASE_RUNNING_TWO:
+                // wait for arriving at the table (during phase transition)
                 break;
         }
 
         DoMeleeAttackIfReady();
     }
 };
-
 CreatureAI* GetAI_boss_professor_putricide(Creature* pCreature)
 {
     return new boss_professor_putricideAI(pCreature);
 }
 
+/**
+ * mob_icc_gas_cloud
+ */
 struct MANGOS_DLL_DECL mob_icc_gas_cloudAI : public ScriptedAI
 {
     mob_icc_gas_cloudAI(Creature* pCreature) : ScriptedAI(pCreature)
@@ -225,12 +471,14 @@ struct MANGOS_DLL_DECL mob_icc_gas_cloudAI : public ScriptedAI
             return;
     }
 };
-
 CreatureAI* GetAI_mob_icc_gas_cloud(Creature* pCreature)
 {
     return new mob_icc_gas_cloudAI(pCreature);
 }
 
+/**
+ * mob_icc_volatile_ooze
+ */
 struct MANGOS_DLL_DECL mob_icc_volatile_oozeAI : public ScriptedAI
 {
     mob_icc_volatile_oozeAI(Creature* pCreature) : ScriptedAI(pCreature)
@@ -248,53 +496,32 @@ struct MANGOS_DLL_DECL mob_icc_volatile_oozeAI : public ScriptedAI
             return;
     }
 };
-
 CreatureAI* GetAI_mob_icc_volatile_ooze(Creature* pCreature)
 {
     return new mob_icc_volatile_oozeAI(pCreature);
 }
 
+// passive mob AI struct
 struct MANGOS_DLL_DECL mob_choking_gas_bombAI : public ScriptedAI
 {
-    mob_choking_gas_bombAI(Creature *pCreature) : ScriptedAI(pCreature)
-    {
-        Reset();
-    }
-
-    void Reset()
-    {
-    }
-
+    mob_choking_gas_bombAI(Creature *pCreature) : ScriptedAI(pCreature){}
+    void Reset(){}
     void AttackStart(Unit *pWho){}
-
-    void UpdateAI(const uint32 uiDiff)
-    {
-    }
+    void UpdateAI(const uint32 uiDiff){}
 };
-
 CreatureAI* GetAI_mob_choking_gas_bomb(Creature* pCreature)
 {
     return new mob_choking_gas_bombAI(pCreature);
 }
 
+// passive mob AI struct
 struct MANGOS_DLL_DECL mob_ooze_puddleAI : public ScriptedAI
 {
-    mob_ooze_puddleAI(Creature *pCreature) : ScriptedAI(pCreature)
-    {
-        Reset();
-    }
-
-    void Reset()
-    {
-    }
-
+    mob_ooze_puddleAI(Creature *pCreature) : ScriptedAI(pCreature){}
+    void Reset(){}
     void AttackStart(Unit *who){}
-
-    void UpdateAI(const uint32 uiDiff)
-    {
-    }
+    void UpdateAI(const uint32 uiDiff){}
 };
-
 CreatureAI* GetAI_mob_ooze_puddle(Creature* pCreature)
 {
     return new mob_ooze_puddleAI(pCreature);
