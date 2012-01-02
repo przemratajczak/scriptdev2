57bdc7e40d1047811f13d92e47daf03949ffc568
Bearq <bear2493@gmail.com>
2012-01-02 23:07:34 +0100

    Change text for Wind Stone

--- DB_Updates/ScriptDev2/sd2_silithus.sql
+++
@@ -1,7 +0,0 @@
-DELETE FROM `gossip_texts` WHERE `entry` IN (-3649011, -3649012, -3649013);
-INSERT INTO `gossip_texts` (`entry`, `content_default`, `comment`) VALUES 
-(-3649011, "A faint whisper is heard form within the stone... Greetings, little one. Do you have news for the Templar?", 'Lesser_Wind_Stone');
-INSERT INTO `gossip_texts` (`entry`, `content_default`, `comment`) VALUES 
-(-3649012, "A clear voice rises form within the stone...You come to us with litle and so we will listen, but do not try our patience. The council has no time for idle gossip from one such as you.", 'Wind_Stone');
-INSERT INTO `gossip_texts` (`entry`, `content_default`, `comment`) VALUES 
-(-3649013, "A thunderous voice bellows form the stone... Greetings, commander. What news of Sillithus do you bring the Lords of the Council?", 'Great_Wind_Stone');
