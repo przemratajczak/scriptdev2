5b01e1ae2f2f7ce513b3ba214e23a397e99376d7
Bearq <bear2493@gmail.com>
2012-01-07 15:43:20 +0100

    Fix quest A Fall of Grace

---
+++ DB_Updates/mangos/quests/sd2_Q_A_Fall_Of_Grace.sql
@@ -0,0 +1,4 @@
+-- Hight abbot
+UPDATE `creature_template` SET `ScriptName` = 'npc_high_abbot_quest' WHERE `entry` = 27245;
+UPDATE `creature_template` SET `ScriptName` = 'npc_high_abbot' WHERE `entry` = 27444;
+UPDATE `creature_template` SET `minhealth` = 39760, `maxhealth` = 39760, `minlevel` = 73, `maxlevel` = 73 WHERE `entry` = 27444;
