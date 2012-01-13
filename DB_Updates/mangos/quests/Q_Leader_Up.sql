5884245a81c97309ad490b3265a20b40668f00d9
Bearq <bear2493@gmail.com>
2012-01-13 15:41:26 +0100

    [a215] Change name for files

---
+++ DB_Updates/mangos/quests/Q_Leader_Up.sql
@@ -0,0 +1,2 @@
+-- Jenny
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `unit_flags` = 512, `flags_extra` = 2, minhealth = 18000, maxhealth = 18000, `ScriptName` = 'npc_jenny' WHERE `entry` = 25969
