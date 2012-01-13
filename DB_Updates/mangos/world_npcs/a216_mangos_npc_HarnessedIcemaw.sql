44d497284f51b70cc5983676d1a4972967fa5108
Bearq <bear2493@gmail.com>
2012-01-13 19:06:33 +0100

    [a217] Add sql for Harnessed Icemaw

--- DB_Updates/mangos/world_npcs/a216_mangos_npc_HarnessedIcemaw.sql
+++ DB_Updates/mangos/world_npcs/a216_mangos_npc_HarnessedIcemaw.sql
@@ -1,2 +1,3 @@
 -- npc_harnessed_icemaw
 UPDATE creature_template SET flags_extra = 4096, ScriptName = "npc_harnessed_icemaw"  WHERE entry = 30468;
+UPDATE `creature_template` SET `minlevel` = 80, `maxlevel` = 80, `minhealth` = 12175, `maxhealth` = 12175, `mechanic_immune_mask` = 75511891 WHERE `entry` = 30468;
