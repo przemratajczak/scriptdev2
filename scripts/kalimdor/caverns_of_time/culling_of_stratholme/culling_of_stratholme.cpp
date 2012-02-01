FallenAngelX/Infinity_MaNGOS

[sd2_mr0308] remove some implicit casts uint64->ObjectGuid

scripts/kalimdor/caverns_of_time/culling_of_stratholme/culling_of_stratholme.cpp
@@ -472,7 +472,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
             case 2:
                 m_creature->SetWalk(true);
                 DoScriptText(SAY_INTRO03, m_creature);
-                m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                 m_creature->GetMotionMaster()->MovePoint(0, 1908.334f, 1315.354f, 149.551f);
                 if (Creature* pUther = m_pInstance->GetSingleCreatureFromStorage(NPC_UTHER))
                     pUther->GetMotionMaster()->MovePoint(0, 1903.600f, 1296.678f, 143.383f);
@@ -562,7 +562,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
                 JumpNextStep(3000);
                 break;
             case 20:
-                m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                 ((npc_arthasAI*)m_creature->AI())->Start(false);
                 JumpNextStep(3000);
                 break;
@@ -611,7 +611,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
                 JumpNextStep(1000);
                 break;
             case 6:
-                m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                 m_creature->GetMotionMaster()->MovePoint(0, 2091.179f,1278.065f,140.476f);
                 DoScriptText(SAY_ENTER06, m_creature);
                 JumpNextStep(3000);
@@ -643,7 +643,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
                 JumpNextStep(7000);
                 break;
             case 11:
-                m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                 DoScriptText(SAY_ENTER10, m_creature);
                 JumpNextStep(12000);
                 break;
@@ -782,10 +782,10 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
                 JumpNextStep(6000);
                 break;
             case 5:
-                m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                 if (Creature* pHuman = m_pInstance->instance->GetCreature(m_uiHuman01GUID))
                 {
-                    pHuman->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                    pHuman->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                     pHuman->UpdateEntry(NPC_INFINITE_ADVERSARY);
                 }
                 if (Creature* pHuman2 = m_pInstance->instance->GetCreature(m_uiHuman02GUID))
@@ -886,7 +886,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
                    Malganis->SetVisibility(VISIBILITY_OFF);
                    m_creature->GetMotionMaster()->MovePoint(0, pMalganis->GetPositionX(), pMalganis->GetPositionY(), pMalganis->GetPositionZ());
                 }
-                m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                 m_creature->SetWalk(false);
                 JumpNextStep(3000);
                 break;

scripts/northrend/borean_tundra.cpp
@@ -552,7 +552,7 @@ struct MANGOS_DLL_DECL npc_nexus_drakeAI : public FollowerAI
 {
     npc_nexus_drakeAI(Creature* pCreature) : FollowerAI(pCreature) { Reset(); }
 
-     uint64 uiHarpoonerGUID;
+     ObjectGuid uiHarpoonerGUID;
      bool bWithRedDragonBlood;
      bool bIsFollowing;
      uint32 SPELL_INTANGIBLE_PRESENCE_Timer;
@@ -575,7 +575,7 @@ struct MANGOS_DLL_DECL npc_nexus_drakeAI : public FollowerAI
      {
             if (pSpell->Id == SPELL_DRAKE_HARPOON && pCaster->GetTypeId() == TYPEID_PLAYER)
             {
-                uiHarpoonerGUID = pCaster->GetGUID();
+                uiHarpoonerGUID = pCaster->GetObjectGuid();
                 DoCast(m_creature, SPELL_RED_DRAGONBLOOD, true);
             }
             m_creature->Attack(pCaster,true);
@@ -592,10 +592,10 @@ struct MANGOS_DLL_DECL npc_nexus_drakeAI : public FollowerAI
            if (Player *pHarpooner = m_creature->GetMap()->GetPlayer(uiHarpoonerGUID))
                  {
 
-                     pHarpooner->KilledMonsterCredit(DRAKE_HUNT_KILL_CREDIT,m_creature->GetGUID());
+                     pHarpooner->KilledMonsterCredit(DRAKE_HUNT_KILL_CREDIT,m_creature->GetObjectGuid());
                      pHarpooner->RemoveAurasByCasterSpell(SPELL_DRAKE_HATCHLING_SUBDUED,uiHarpoonerGUID);
                      SetFollowComplete();
-                     uiHarpoonerGUID = 0;
+                     uiHarpoonerGUID.Clear();
                      m_creature->ForcedDespawn(1000);
                  }
 
@@ -669,7 +669,7 @@ bool GOHello_go_scourge_cage(Player* pPlayer, GameObject* pGo)
         Creature *pCreature = GetClosestCreatureWithEntry(pGo, NPC_SCOURGE_PRISONER, INTERACTION_DISTANCE);
         if(pCreature)
         {
-            pPlayer->KilledMonsterCredit(NPC_SCOURGE_PRISONER, pCreature->GetGUID());
+            pPlayer->KilledMonsterCredit(NPC_SCOURGE_PRISONER, pCreature->GetObjectGuid());
             pCreature->CastSpell(pCreature, 43014, false);
         }
     }
@@ -701,7 +701,7 @@ struct MANGOS_DLL_DECL npc_beryl_sorcererAI : public FollowerAI
     }
 
     bool bEnslaved;
-    uint64 uiChainerGUID;
+    ObjectGuid uiChainerGUID;
     uint32 m_uiNormalFaction;
 
     uint32 SPELL_FROST_BOLT_Timer;
@@ -725,7 +725,7 @@ struct MANGOS_DLL_DECL npc_beryl_sorcererAI : public FollowerAI
             {
                 EnterEvadeMode(); //We make sure that the npc is not attacking the player!
                 m_creature->setFaction(35);
-                uiChainerGUID = pCaster->GetGUID();
+                uiChainerGUID = pCaster->GetObjectGuid();
                 if(Player *pChainer = m_creature->GetMap()->GetPlayer(uiChainerGUID))
                 {
                 StartFollow(pChainer, 35, NULL);
@@ -744,7 +744,7 @@ struct MANGOS_DLL_DECL npc_beryl_sorcererAI : public FollowerAI
             {
                 if(Player *pChainer = m_creature->GetMap()->GetPlayer(uiChainerGUID))
                 {
-                    pChainer->KilledMonsterCredit(NPC_CAPTURED_BERLY_SORCERER,m_creature->GetGUID());
+                    pChainer->KilledMonsterCredit(NPC_CAPTURED_BERLY_SORCERER,m_creature->GetObjectGuid());
                     SetFollowComplete();
                     m_creature->ForcedDespawn(1000);
                 }

przemratajczak/scriptdev2

Conflicts:
	scripts/kalimdor/caverns_of_time/culling_of_stratholme/culling_of_stratholme.cpp
	scripts/kalimdor/caverns_of_time/culling_of_stratholme/culling_of_stratholmeai.cpp
	scripts/northrend/borean_tundra.cpp
	sd2_revision_R2.h

scripts/kalimdor/caverns_of_time/culling_of_stratholme/culling_of_stratholme.cpp
@@ -502,14 +502,14 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
           case 2:
              m_creature->SetWalk(true);
              DoScriptText(SAY_INTRO03, m_creature);
-             m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+             m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
              m_creature->GetMotionMaster()->MovePoint(0, 1908.334f, 1315.354f, 149.551f);
-             if(Creature* pUther = m_pInstance->instance->GetCreature(NPC_UTHER))
+             if(Creature* pUther = m_pInstance->GetSingleCreatureFromStorage(NPC_UTHER))
                 pUther->GetMotionMaster()->MovePoint(0, 1903.600f, 1296.678f, 143.383f);
              JumpNextStep(2000);
              break;
           case 3:
-             if(Creature* pJaina = m_pInstance->instance->GetCreature(NPC_JAINA))
+              if(Creature* pJaina = m_pInstance->GetSingleCreatureFromStorage(NPC_JAINA))
                 pJaina->GetMotionMaster()->MovePoint(0, 1899.641f, 1298.684f, 143.831f);
              JumpNextStep(7000);
              break;
@@ -523,18 +523,18 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
              JumpNextStep(10000);
              break;
           case 6:
-             if(Creature* pUther = m_pInstance->instance->GetCreature(NPC_UTHER))
+             if(Creature* pUther = m_pInstance->GetSingleCreatureFromStorage(NPC_UTHER))
                 DoScriptText(SAY_INTRO05, pUther);
              JumpNextStep(1000);
              break;
           case 7:
-             if(Creature* pUther = m_pInstance->instance->GetCreature(NPC_UTHER))
+             if(Creature* pUther = m_pInstance->GetSingleCreatureFromStorage(NPC_UTHER))
                 m_creature->SetGuidValue(UNIT_FIELD_TARGET, pUther->GetObjectGuid());
              DoScriptText(SAY_INTRO06, m_creature);
              JumpNextStep(4000);
              break;
           case 8:
-             if(Creature* pUther = m_pInstance->instance->GetCreature(NPC_UTHER))
+             if(Creature* pUther = m_pInstance->GetSingleCreatureFromStorage(NPC_UTHER))
                 DoScriptText(SAY_INTRO07, pUther);
              JumpNextStep(6000);
              break;
@@ -543,7 +543,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
              JumpNextStep(4000);
              break;
           case 10:
-             if(Creature* pUther = m_pInstance->instance->GetCreature(NPC_UTHER))
+             if(Creature* pUther = m_pInstance->GetSingleCreatureFromStorage(NPC_UTHER))
                 DoScriptText(SAY_INTRO09, pUther);
              JumpNextStep(8000);
              break;
@@ -552,7 +552,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
              JumpNextStep(4000);
              break;
           case 12:
-             if(Creature* pUther = m_pInstance->instance->GetCreature(NPC_UTHER))
+             if(Creature* pUther = m_pInstance->GetSingleCreatureFromStorage(NPC_UTHER))
                 DoScriptText(SAY_INTRO11, pUther);
              JumpNextStep(4000);
              break;
@@ -561,7 +561,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
              JumpNextStep(11000);
              break;
           case 14:
-             if(Creature* pJaina = m_pInstance->instance->GetCreature(NPC_JAINA))
+             if(Creature* pJaina = m_pInstance->GetSingleCreatureFromStorage(NPC_JAINA))
                 DoScriptText(SAY_INTRO13, pJaina);
              JumpNextStep(3000);
              break;
@@ -570,12 +570,12 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
              JumpNextStep(9000);
              break;
           case 16:
-             if(Creature* pUther = m_pInstance->instance->GetCreature(NPC_UTHER))
+             if(Creature* pUther = m_pInstance->GetSingleCreatureFromStorage(NPC_UTHER))
                 DoScriptText(SAY_INTRO15, pUther);
              JumpNextStep(5000);
              break;
           case 17:
-             if(Creature* pJaina = m_pInstance->instance->GetCreature(NPC_JAINA))
+             if(Creature* pJaina = m_pInstance->GetSingleCreatureFromStorage(NPC_JAINA))
              {
                 m_creature->SetGuidValue(UNIT_FIELD_TARGET, pJaina->GetObjectGuid());
                 pJaina->GetMotionMaster()->MovePoint(0, 1794.357f,1272.183f,140.558f);
@@ -587,12 +587,12 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
              JumpNextStep(1000);
              break;
           case 19:
-             if(Creature* pJaina = m_pInstance->instance->GetCreature(NPC_JAINA))
+             if(Creature* pJaina = m_pInstance->GetSingleCreatureFromStorage(NPC_JAINA))
                 DoScriptText(SAY_INTRO17, pJaina);
              JumpNextStep(3000);
              break;
           case 20:
-             m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+             m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
              ((npc_arthasAI*)m_creature->AI())->Start(false);
              JumpNextStep(3000);
              break;
@@ -641,7 +641,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
               JumpNextStep(1000);
               break;
           case 6:
-              m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+              m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
               m_creature->GetMotionMaster()->MovePoint(0, 2091.179f,1278.065f,140.476f);
               DoScriptText(SAY_ENTER06, m_creature);
               JumpNextStep(3000);
@@ -673,7 +673,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
               JumpNextStep(7000);
               break;
           case 11:
-              m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+              m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
               DoScriptText(SAY_ENTER10, m_creature);
               JumpNextStep(12000);
               break;
@@ -817,10 +817,10 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
              JumpNextStep(6000);
              break;
           case 5:
-             m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+             m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
              if(Creature* Human = m_pInstance->instance->GetCreature(m_uiHuman01GUID))
              {
-                Human->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                Human->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                 Human->UpdateEntry(NPC_INFINITE_ADVERSARY);
              }
              if(Creature* Human2 = m_pInstance->instance->GetCreature(m_uiHuman02GUID))
@@ -921,7 +921,7 @@ struct MANGOS_DLL_DECL npc_arthasAI : public npc_escortAI
                    Malganis->SetVisibility(VISIBILITY_OFF);
                    m_creature->GetMotionMaster()->MovePoint(0, Malganis->GetPositionX(), Malganis->GetPositionY(), Malganis->GetPositionZ());
                 }
-                m_creature->SetGuidValue(UNIT_FIELD_TARGET, 0);
+                m_creature->SetGuidValue(UNIT_FIELD_TARGET, ObjectGuid());
                 m_creature->SetWalk(false);
                 JumpNextStep(3000);
                 break;

scripts/northrend/borean_tundra.cpp
@@ -952,7 +952,6 @@ enum
 struct MANGOS_DLL_DECL npc_nexus_drakeAI : public FollowerAI
 {
     npc_nexus_drakeAI(Creature* pCreature) : FollowerAI(pCreature) { Reset(); }
-    
      ObjectGuid uiHarpoonerGuid;
      bool bWithRedDragonBlood;
      bool bIsFollowing;
@@ -992,11 +991,10 @@ struct MANGOS_DLL_DECL npc_nexus_drakeAI : public FollowerAI
          {
            if (Player *pHarpooner = m_creature->GetMap()->GetPlayer(uiHarpoonerGuid))
                  {
-                    
                      pHarpooner->KilledMonsterCredit(DRAKE_HUNT_KILL_CREDIT, m_creature->GetObjectGuid());
                      pHarpooner->RemoveAurasByCasterSpell(SPELL_DRAKE_HATCHLING_SUBDUED, uiHarpoonerGuid);
                      SetFollowComplete();
-                     uiHarpoonerGuid = 0;
+                     uiHarpoonerGuid.Clear();
                      m_creature->ForcedDespawn(1000);
                  }
 
@@ -1237,10 +1235,10 @@ struct MANGOS_DLL_DECL npc_thassarianAI : public ScriptedAI
 
     void Reset()
     {
-        m_uiValanarGuid = 0;
-        m_uiLichKingGuid = 0;
-        m_uiLeryssaGuid = 0;
-        m_uiArlosGuid = 0;
+        m_uiValanarGuid.Clear();
+        m_uiLichKingGuid.Clear();
+        m_uiLeryssaGuid.Clear();
+        m_uiArlosGuid.Clear();
         m_creature->SetFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_GOSSIP);
         if (m_creature->GetAreaId() == AREA_ID)
             m_creature->RemoveFlag(UNIT_NPC_FLAGS, UNIT_NPC_FLAG_QUESTGIVER);