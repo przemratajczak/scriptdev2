10d58c53569f43be0c0de0165b7ca8085c2a5fd3
przemratajczak <przemratajczak@gmail.com>
2013-03-01 19:02:31 +0100

    fixed sql from prev commit

--- DB_Updates/ScriptDev2/sd2_s358_q_LastOfHerKind.sql
+++ DB_Updates/ScriptDev2/sd2_s358_q_LastOfHerKind.sql
@@ -1,6 +1,6 @@
 /********************** MaNGOS DB **********************/
 -- set injured matriarch as spell_click creature, no other flags needed
-UPDATE creature_template SET npcflag = 16777216 WHERE entry = 29563;
+UPDATE creature_template SET npcflag = 16777216, ScriptName = 'npc_harnessed_icemaw_matriarch' WHERE entry = 29563;
 
 DELETE FROM npc_spellclick_spells WHERE npc_entry = 29563;
 INSERT INTO npc_spellclick_spells (npc_entry, spell_id, quest_start,quest_start_active,quest_end,cast_flags) VALUES
